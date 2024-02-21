6f33365ac868253ac5dbd54d768831e584350137
K-Tamas01 <korom01tamas@gmail.com>
2024-02-21 16:23:38 +0100

    add Playlist

--- MusicHandler/Manager/Playlist.js
+++ MusicHandler/Manager/Playlist.js
@@ -1,7 +1,29 @@
-const EventEmmitter = require('events')
+const play_dl = require('play-dl')
+const { createAudioResource } = require('@discordjs/voice')
 
-class PlayList extends EventEmmitter{
+class PlayList{
+    constructor(){
+        this.options = {
+            quality: 2,
+            format: 'mp3',
+        }
+    }
 
+    async play(content){
+        const is_url = content.startsWith('http')
+        const resource = []
+        if(is_url){
+            const playlistInfo = await play_dl.playlist_info(content)
+            for(let track of playlistInfo.tracks){
+                const stream = await play_dl.stream(track, this.options)
+                resource.push(createAudioResource(stream.stream, {
+                    inlineVolume: true,
+                    inputType: stream.type
+                }))
+            }
+        }
+        return resource
+    }
 }
 
 module.exports = PlayList
