d5294a2ee0d9f92c2ac8950deade368faad36989
K-Tamas01 <korom01tamas@gmail.com>
2024-02-21 16:28:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const Discord = require('discord.js');
-const { createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
-const play_yt = require('play-dl')
-const StreamConnection = require('./MusicHandler/Utils/StreamConnection');
 const Player = require('./MusicHandler/Player')
 const bot = new Discord.Client({
 	intents: [
@@ -14,30 +11,10 @@ const bot = new Discord.Client({
 });
 
 bot.player = new Player()
-const streamConnection = new StreamConnection()
-const voiceConnection = {}
-const options = {
-	quality: 2,
-	format: 'mp3',
-}
 
 bot.on('messageCreate', async (msg) => {
-	// const player = createAudioPlayer();
-	// const stream = await play_yt.stream(msg.content, options)
-	// const resource = createAudioResource(stream.stream, {
-	// 	inlineVolume: true,
-	// 	inputType: stream.type
-	// })
-
 	bot.player.join(msg)
 	bot.player.play(msg.content, msg.guild.id)	
-	
-	// await voiceConnection[msg.member.guild.id].player.play(resource)
-	// voiceConnection[msg.member.guild.id].player.on(AudioPlayerStatus.Idle, () => {
-	// 	streamConnection.leave(voiceConnection[msg.member.guild.id].connection)
-	// 	voiceConnection[msg.member.guild.id].player.stop()
-	// 	delete voiceConnection[msg.member.guild.id]
-	// })
 })
 
 bot.on('ready', () => {
