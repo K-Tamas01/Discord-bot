bb332c680421bc04199d99d96b1864fb7a5b89da
K-Tamas01 <korom01tamas@gmail.com>
2024-02-22 21:37:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const {prefix, token} = require('./Config/config.json')
 const Player = require('./MusicHandler/Player')
 const bot = new Discord.Client({
 	intents: [
@@ -9,13 +10,51 @@ const bot = new Discord.Client({
 		Discord.IntentsBitField.Flags.GuildVoiceStates,
 	],
 });
+const fs = require('fs');
 
 bot.player = new Player()
+bot.command = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+bot.categ = fs.readdirSync('./Handler/Commands');
+['Handler'].forEach(handler => {
+	require(`./Handler/${handler}`)(bot);
+});
 
 bot.on('messageCreate', async (msg) => {
-	bot.player.join(msg)
-	bot.player.play(msg.content, msg.guild.id)	
+	if(msg.author.bot) return;
+	if(!msg.content.startsWith(prefix)) return;
+
+	const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+	let cmd = args.shift().toLowerCase();
+
+	if (cmd === 0) cmd = 'help';
+
+	let command = bot.command.get(cmd);
+
+	if (!command) command = bot.command.get(bot.aliases.get(cmd));
+
+	if (command === undefined) {
+		return;
+	}
+
+	command.run(bot, msg, args);
 })
+
+bot.player.on('firstSong', () => {
+
+})
+
+bot.player.on('songChanged', () => {
+
+})
+
+bot.player.on('queueEnd', () => {
+
+})
+
+// bot.player.on('queueDestroy', () => {
+
+// })
 
 bot.on('ready', () => {
 	console.log('Online')
@@ -33,4 +72,5 @@ bot.on('ready', () => {
 	}, 5000)
 })
 
-bot.login("OTE4NTU1NTUyMzA3NjI2MDI1.GJHCM7.QxbGnQvcY2ZE-NiDcYXG7euVF-xMVbPj-7hirc")
+bot.login(token)
+//bot.login("OTE1MTU3NDExMTg5Mzc1MDQ3.Gf8s1M.n1eKU9kOxROlu2DUUfOHtXvKCDcMv5XiqRSfrU") //Ez az éles bot tokenje NE HASZNÁLD
