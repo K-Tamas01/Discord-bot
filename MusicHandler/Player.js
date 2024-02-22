673ca25e7e9b5c72c76b60f3f8dd1286cd5a0d53
K-Tamas01 <korom01tamas@gmail.com>
2024-02-22 18:52:13 +0100

    add events

--- MusicHandler/Player.js
+++ MusicHandler/Player.js
@@ -1,11 +1,13 @@
+const EventEmitter = require('events')
 const Queue = require('./Manager/Queue')
 const StreamConnection = require('./Utils/StreamConnection')
 const Song = require('./Manager/Song')
 const Playlist = require('./Manager/Playlist')
 const { createAudioPlayer } = require('@discordjs/voice')
 
-class Player{
+class Player extends EventEmitter{
     constructor(){
+        super()
         this.StreamConnectionCollection = {}
         this.StreamConnection = new StreamConnection()
         this.Song = new Song()
@@ -46,12 +48,15 @@ class Player{
         const resource = await this.Song.play(content)
         if(this.StreamConnectionCollection[guildId].player.state.status === 'idle') {
             this.StreamConnectionCollection[guildId].player.play(resource)
+            this.emit('firstSong')
             this.StreamConnectionCollection[guildId].status = this.StreamConnectionCollection[guildId].player.on('stateChange', (oldStatus, newStatus) => {
                 if(newStatus.status === 'idle' && this.StreamConnectionCollection[guildId].queue.size() > 0){
                     this.StreamConnectionCollection[guildId].player.play(this.StreamConnectionCollection[guildId].queue.dequeue())
+                    this.emit('songChanged')
                 }
                 else if(newStatus.status === 'idle' && oldStatus.status !== 'idle'){
                     this.destroyConnection(guildId)
+                    this.emit('queueEnd')
                 }
             })
         } else {
@@ -63,12 +68,15 @@ class Player{
         const resources = await this.Playlist.play(content)
         if(this.StreamConnectionCollection[guildId].player.state.status === 'idle') {
             this.StreamConnectionCollection[guildId].player.play(resources.shift())
+            this.emit('firstSong')
             this.StreamConnectionCollection[guildId].status = this.StreamConnectionCollection[guildId].player.on('stateChange', (oldStatus, newStatus) => {
                 if(newStatus.status === 'idle' && this.StreamConnectionCollection[guildId].queue.size() > 0){
                     this.StreamConnectionCollection[guildId].player.play(this.StreamConnectionCollection[guildId].queue.dequeue())
+                    this.emit('songChanged')
                 }
                 else if(newStatus.status === 'idle' && oldStatus.status !== 'idle'){
                     this.destroyConnection(guildId)
+                    this.emit('queueEnd')
                 }
             })
         }
