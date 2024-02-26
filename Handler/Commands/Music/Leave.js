98de8ebb80231d4aeb13a38fc9f3bec603cf0a69
K-Tamas01 <korom01tamas@gmail.com>
2024-02-26 14:33:50 +0100

    add leave

--- Handler/Commands/Music/Leave.js
+++ Handler/Commands/Music/Leave.js
@@ -4,5 +4,10 @@ module.exports = {
 	description:'$leave',
 	isload: true,
 	run: async (bot, msg, args, guildqueue) => {
+		if(msg.member.voice.channel !== null){
+			if(msg.guild.members.me.voice.channel === null || msg.guild.members.me.voice.channel.id === msg.member.voice.channel.id){
+				bot.player.destroyConnection(msg.guild.id)
+			}
+		}
     }
 }
