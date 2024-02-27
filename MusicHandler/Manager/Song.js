b8986513c907c54d50539e9454c2f6ebf1fcd5d2
K-Tamas01 <korom01tamas@gmail.com>
2024-02-27 08:56:18 +0100

    Update Song.js

--- MusicHandler/Manager/Song.js
+++ MusicHandler/Manager/Song.js
@@ -6,17 +6,12 @@ class Song{
         const is_url = content.startsWith('https')
         if(is_url){
             const searchInfo = await play_yt.search(content, {source: { youtube: "video"}, limit: 1 })
-
+            
             return searchInfo
         } else {
             const searchInfo = await play_yt.search(content, { source : { youtube : "video" }, limit: 1 })
-            const stream = await play_yt.stream(searchInfo[0].url, this.options)
-            const resource = createAudioResource(stream.stream, {
-                inlineVolume: true,
-                inputType: stream.type
-            })
 
-            return resource
+            return searchInfo
         }
     }
 }
