daefa3b50af84bdd3d4fe51408996a578d17928d
K-Tamas01 <korom01tamas@gmail.com>
2024-03-06 11:02:22 +0100

    Queue loop

--- Handler/Commands/Music/Queue.js
+++ Handler/Commands/Music/Queue.js
@@ -8,7 +8,7 @@ module.exports = {
 	run: async (bot, msg) => {
 		if(msg.member.voice.channel !== null){
 			const data = await bot.player.getQueue(msg.guild.id)
-			suc.run(msg, 1, data, undefined, msg.author.username)
+			suc.run(msg, 1, data, undefined, msg.author ? msg.author.username : 'Bot')
 		}
     }
 }
