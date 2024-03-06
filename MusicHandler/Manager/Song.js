2c63c585505790b5c1da1744d56c8eeb39471554
K-Tamas01 <korom01tamas@gmail.com>
2024-03-06 11:02:44 +0100

    Update Song.js

--- MusicHandler/Manager/Song.js
+++ MusicHandler/Manager/Song.js
@@ -3,16 +3,8 @@ const play_yt = require('play-dl')
 class Song{
 
     async play(content){
-        const is_url = content.startsWith('https')
-        if(is_url){
             const searchInfo = await play_yt.search(content, {source: { youtube: "video"}, limit: 1 })
-            
             return searchInfo
-        } else {
-            const searchInfo = await play_yt.search(content, { source : { youtube : "video" }, limit: 1 })
-
-            return searchInfo
-        }
     }
 }
 
