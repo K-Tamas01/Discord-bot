2fc0d2938d2acdff9a5e5d7735ee3f91adaa02a5
K-Tamas01 <korom01tamas@gmail.com>
2024-03-07 10:13:53 +0100

    Update Song.js

--- MusicHandler/Manager/Song.js
+++ MusicHandler/Manager/Song.js
@@ -3,8 +3,30 @@ const play_yt = require('play-dl')
 class Song{
 
     async play(content){
-            const searchInfo = await play_yt.search(content, {source: { youtube: "video"}, limit: 1 })
-            return searchInfo
+        const limit = content.startsWith('https') ? 1 : 20
+        const searchInfo = await play_yt.search(content, {source: { youtube: "video"}, limit: limit })
+        return searchInfo.length > 1 ? this.greatestFit(searchInfo, content) : searchInfo
+    }
+
+    greatestFit(searchResult, content){
+        let bestMatching = 0
+        let bestFittingIndex = -1
+        let index = 0
+        for(let element of searchResult){
+            let length = 0
+            for(let i = 0; i < Math.min(element.title.length, content.length); i++){
+                if(element.title.includes(content.substring(0, i + 1))){
+                    length += 1 
+                }
+            }
+            if(length > bestMatching){
+                bestMatching = length
+                bestFittingIndex = index
+            }
+            index++
+        }
+
+        return [searchResult[bestFittingIndex]]
     }
 }
 
