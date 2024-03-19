7c95e99e8751a1f1e0206863a3a2454d25d1f3eb
K-Tamas01 <korom01tamas@gmail.com>
2024-03-19 08:45:40 +0100

    Update Song.js

--- MusicHandler/Manager/Song.js
+++ MusicHandler/Manager/Song.js
@@ -1,11 +1,37 @@
 const play_yt = require('play-dl')
 
 class Song{
+    constructor(){
+        this.searchInfo = [{
+            url: undefined,
+            type: undefined,
+            title: undefined,
+            duration: undefined
+        }]
+    }
 
     async play(content){
-        const limit = content.startsWith('https') ? 1 : 20
-        const searchInfo = await play_yt.search(content, {source: { youtube: "video"}, limit: limit })
-        return searchInfo.length > 1 ? this.greatestFit(searchInfo, content) : searchInfo
+        if(content.startsWith('https')){
+            const data = await play_yt.video_basic_info(content)
+            this.searchInfo[0] = {
+                url: data.video_details.url,
+                type: data.video_details.type,
+                title: data.video_details.title,
+                duration: data.video_details.durationRaw
+            }
+        } else {
+            const searchResult = await play_yt.search(content, {source: { youtube: "video"}, limit: 20 })
+            const result = this.greatestFit(searchResult, content)
+            console.log(result[0].url)
+            this.searchInfo[0] = {
+                url: result[0].url,
+                type: result[0].type,
+                title: result[0].title,
+                duration: result[0].durationRaw
+            }
+        }
+
+        return this.searchInfo
     }
 
     greatestFit(searchResult, content){
