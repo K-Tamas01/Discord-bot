91345c5a95e241563c1613b7dea9fea118eb69c8
K-Tamas01 <korom01tamas@gmail.com>
2024-03-19 08:45:22 +0100

    Update Playlist.js

--- MusicHandler/Manager/Playlist.js
+++ MusicHandler/Manager/Playlist.js
@@ -3,8 +3,7 @@ const play_dl = require('play-dl')
 class PlayList{
 
     async play(content){
-        const is_url = content.startsWith('http')
-        if(is_url){
+        if(content.startsWith('http')){
             const playlistInfo = await play_dl.playlist_info(content, {incomplete: true})
             return playlistInfo
         }
